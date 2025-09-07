ad7e7dd0acd89b0fe3c1990d860494b5f6771fef
Tatenda Loverage Tirivavi <tloverage@gmail.com>
2025-09-07 21:25:01 +0200

    Add styled homepage template

--- event_management/views.py
+++ event_management/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("Welcome to the Event Management Project 🚀")
+    return render(request, "home.html")
